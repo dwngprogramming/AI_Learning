0aaa7a6a011c0fdeb8aaab451c3a2c8e3af94933
dwngprogramming <dwnq.coding@gmail.com>
2023-10-31 00:39:53 +0700

    Commit bai tap

--- src/lab_day1/task3/TestSimpleReflexAgent.java
+++ src/lab_day1/task3/TestSimpleReflexAgent.java
@@ -2,10 +2,10 @@ package lab_day1.task3;
 
 public class TestSimpleReflexAgent {
 	public static void main(String[] args) {
-		Environment env = new Environment(3, 4, 0.2);
+		Environment env = new Environment(2, 2, 0.2);
 		Agent agent = new Agent(new AgentProgram());
-		// Do not new Location because it makes a difference reference
-		env.addAgent(agent, new Location(1, 1));
+		// Do not new Location because it makes a difference reference in HashMap
+		env.addAgent(agent, 1, 1);
 
 		env.stepUntilDone();
 	}
